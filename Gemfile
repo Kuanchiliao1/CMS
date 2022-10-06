ae05114d993cbdcf3ddc6a09f529f149a89460d4
kuanchiliao1 <kuanchiliao@gmail.com>
2022-10-06 10:23:27 -0400

    Update Ruby V2.7.2 => 3.1.2

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 source "https://rubygems.org"
+ruby "3.1.2"
 
 gem "sinatra"
 gem "sinatra-contrib" # Contains reloader module
 gem "erubis"
 gem "pry"
+gem "redcarpet"
+gem "puma"
 
 gem "rack-test"
 gem "minitest"
--- cms.rb
+++ cms.rb
@@ -5,9 +5,13 @@ require "pry"
 
 root = File.expand_path("..", __FILE__)
 
+# Looks like I need to include `configure` or else I get a 32 byte argument error
 configure do
   enable :sessions
   set :session_secret, 'A\xFD\xBC\xAasdfasdfasdfasdfasdfasdffweaefsadfsadfasdfasdfasd5\x1A\x8E\xD7\x17W\x00r5\x8CHv\xA7\xFB6\xB8N\x9Fb\x93\xA4\x9Aw\x8E\bq\xBA\xFC\xEF\xA3\x9E\xE2\xED\xB1\b\xBC\xE3\xDA\xEA\xDB\xF2\xAC0\xDAh\xCE\x88/(\x16\xC9\xDD'
+end
+
+before do
 end
 
 get "/" do
